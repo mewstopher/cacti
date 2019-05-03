db58ebdff1d063b808fa864607d38eba5383c4d3
Mu <mu@email.com>
2019-05-03 00:56:24 -0400

    adds to forward prop

--- code/tools.py
+++ code/tools.py
@@ -66,7 +66,22 @@ def forward_propogation(X, parameters):
 
     define model here
     """
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+
+    Z1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1], padding='SAME')
+    A1 = tf.nn.relu(Z1)
+    P1 = tf.nn.max_pool(A1, ksize[1,8,8,1], strides=[1,8,8,1],
+            padding='SAME')
     
+    Z2 = tf.nn.conv2d(P1, W2, strides=[1,1,1,1], padding='SAME')
+    A2 = tf.nn.relu(Z2)
+    P2 = tf.nn.max_pool(A2, ksize=[1,4,4,1], 
+            strides=[1,4,4,1],padding='SAME')
+    P2 = tf.nn.contrib.layers.flatten(P2)
+    Z3 = tf.nn.contrib.layers.fully_connected(P2, num_classes,
+            activation_fn = None
+
     return Z3
 
 
