27f409b5f9ba881b158000f39505d69c37360311
Mu <mu@email.com>
2019-05-04 21:50:28 -0400

    works on vg16

--- code/tools.py
+++ code/tools.py
@@ -71,12 +71,20 @@ def initialize_parameters():
     shapes are the shape of filters/weights
     """
 
-    W1 = tf.get_variable('W1', [4,4,3,8], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
-    W2 = tf.get_variable('W2', [2,2,8,16], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W1 = tf.get_variable('W1', [3,3,3,64], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W2 = tf.get_variable('W2', [3,3,64,128], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W3 = tf.get_variable('W3', [3,3,128,256], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W4 = tf.get_variable('W4', [3,3,256,512], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+    W5 = tf.get_variable('W5', [3,3,512,512], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
+
     ### END CODE HERE ###
 
     parameters = {"W1": W1,
-            "W2": W2}
+            "W2": W2,
+            "W3": W3,
+            "W4": W4,
+            "W5": W5
+            }
 
     return parameters
 
@@ -89,29 +97,41 @@ def forward_propogation(X, parameters):
     """
     W1 = parameters['W1']
     W2 = parameters['W2']
+    W3 = parameters['W3']
+    W4 = parameters['W4']
+    W5 = parameters['W5']
 
     Z1 = tf.nn.conv2d(X, W1, strides=[1,1,1,1], padding='SAME')
     A1 = tf.nn.relu(Z1)
-    P1 = tf.nn.max_pool(A1, ksize=[1,8,8,1], strides=[1,8,8,1],
+    P1 = tf.nn.max_pool(A1, ksize=[1,2,2,1], strides=[1,2,2,1],
             padding='SAME')
     
     Z2 = tf.nn.conv2d(P1, W2, strides=[1,1,1,1], padding='SAME')
     A2 = tf.nn.relu(Z2)
-    P2 = tf.nn.max_pool(A2, ksize=[1,4,4,1], 
-            strides=[1,4,4,1],padding='SAME')
-    P2 = tf.contrib.layers.flatten(P2)
-    Z3 = tf.contrib.layers.fully_connected(P2, 2,
+    P2 = tf.nn.max_pool(A2, ksize=[1,2,2,1], 
+            strides=[1,2,2,1],padding='SAME')
+    Z3 = tf.nn.conv2d(P2, W3, strides=[1,1,1,1], padding='SAME')
+    A3 = tf.nn.relu(Z3)
+    P3 = tf.nn.max_pool(A3, strides=[1,1,1,1],
+            ksize=[1,2,2,1], padding='SAME')
+
+    z4 = tf.nn.conv2d(p3, W4, strides=[1,1,1,1], padding='SAME')
+    A4 = tf.nn.relu(Z4)
+    P4 = tf.nn.max_pool(A4, strides=[1,1,1,1],
+            ksize=[1,2,2,1])
+    P4 = tf.contrib.layers.flatten(P2)
+    Z4 = tf.contrib.layers.fully_connected(P2, 2,
             activation_fn = None)
 
-    return Z3
+    return Z4
 
 
-def compute_cost(Z3, Y):
+def compute_cost(Z4, Y):
     """
     computes cost for output layer
     """
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=Y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z4, labels=Y))
 
     return cost
 
@@ -141,9 +161,9 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=.009,
 
     parameters = initialize_parameters()
 
-    Z3 = forward_propogation(X, parameters)
+    Z4 = forward_propogation(X, parameters)
 
-    cost = compute_cost(Z3, Y)
+    cost = compute_cost(Z4, Y)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
